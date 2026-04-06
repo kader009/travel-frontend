2cee58c36b614085706c637c39ebc6574416353b
Md Abdul Kader Molla <kadermollarasel000@gmail.com>
2026-04-06 17:05:58 +0600

    testimonial pagination darm mode active bg color issue fix

--- src/home/TestimonialSwiper.tsx
+++ src/home/TestimonialSwiper.tsx
@@ -34,7 +34,7 @@ const TestimonialSwiper = ({
           {testimonials.map((_, index) => (
             <div
               key={index}
-              className={`h-1 w-8! sm:w-12! rounded transition-colors duration-300 ${activeIndex === index ? 'bg-primary' : 'bg-slate-200 dark:bg-background-dark'}`}
+              className={`h-1 w-8! sm:w-12! rounded transition-colors duration-300 ${activeIndex === index ? 'bg-primary' : 'bg-slate-200 dark:bg-white'}`}
             ></div>
           ))}
         </div>
